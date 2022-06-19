a0dd78ab460395256e772cb212560aba2f9b5ce8
Alvaro Padilla <monster_what@hotmail.com>
2022-06-19 14:23:59 -0600

    Instrucciones para vacaciones.

--- src/java/soap/services/wb_Vacaciones.java
+++ src/java/soap/services/wb_Vacaciones.java
@@ -13,6 +13,11 @@ public class wb_Vacaciones {
      */
     
     // Son 2 semanas de vacaciones por cada 50 trabajadas continuamente.
+    // 50 semanas son 300 dias
+    // Salario promedio/30 = Salario Diario
+    // 16 meses en adelante.
+    // if(antiguedad_empleado < 15){sout = no tiene vacaciones.}
+    // if(antiguedad_empleado >= 16){mate para calcular cuanto tiempo/pago}
     @WebMethod(operationName = "hello")
     public String hello(@WebParam(name = "name") String txt) {
         return "Hello " + txt + " !";
